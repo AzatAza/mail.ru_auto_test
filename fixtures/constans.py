19a7d5b63ad4d00c83c4eed95350fc4fd00b63e9
AzatAza <ai.allagulov@yandex.ru>
2022-02-27 21:28:37 +0600

    update constants

--- fixtures/constans.py
+++ fixtures/constans.py
@@ -1,18 +1,2 @@
 class Constants:
     LETTER_SENT = 'Письмо отправлено'
-
-
-
-    # LOGIN_ERROR_MESSAGE = 'Неверный логин или пароль, попробуйте заново.'
-    # EMPTY_USERNAME_MESSAGE = '- Не указан логин'
-    # EMPTY_PASSWORD_MESSAGE = '- Заполните поле'
-    # EMPTY_EMAIL_MESSAGE = '- Заполните поле'
-    # EMPTY_FIRSTNAME_MESSAGE = '- Заполните поле'
-    # EMPTY_LASTNAME_MESSAGE = '- Заполните поле'
-    # LOWERCASE_USERNAME_ERROR_MESSAGE = 'Допустимы только строчные буквы'
-    # PASSWORD_LESS_THAN_8 = 'Пароль не должен состоять из менее чем 8 символов(а).'
-    # PASSWORD_NOT_A_DIGIT = 'Пароль должен содержать не менее 1 цифр(ы).'
-    # PASSWORD_NOT_LOWERCASE = 'Пароль должен содержать не менее 1 строчных(ой) букв(ы).'
-    # PASSWORD_NOT_UPPERCASE = 'Пароль должен содержать не менее 1 прописных(ой) букв(ы).'
-    # PASSWORD_NOT_A_SPEC_SYMBOL = 'Пароль должен содержать не менее 1 символов, не являющихся буквами и цифрами, ' \
-    #                              'например таких как *, - или #.'
